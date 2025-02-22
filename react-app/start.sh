ce23c5aa25c46a08b94da8f677e98fb14eb27486
Aryan Wadhwa <73252745+aryanwad@users.noreply.github.com>
2025-02-22 14:19:01 -0500

    Update start.sh

--- react-app/start.sh
+++ react-app/start.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+echo "Starting Flask..."
+cd ../python-backend  # Adjust this path based on where yelp-and-speak.py is located
+python yelp-and-speak.py &  # Run Flask in the background
 
+echo "Starting React..."
+cd ../react-app  # Adjust this path if needed
+npm start
